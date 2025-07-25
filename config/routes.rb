44c793ad1b34389acdccede993f81fa4aee3c44b
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-07-25 03:32:49 +0000

    finished dice_roller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-
+  get("/", {controller: "dice_roller", action: "homepage"})
+  get("/dice/:number_of_dice/:how_many_sides", {controller: "dice_roller", action: "flexible_dice"})
 end
